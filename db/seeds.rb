12bbab6d28295969ed9461d2b14ec725a845f591
Jake Buller <jake.buller@enflick.com>
2015-03-29 09:42:31 -0400

    Clean up seeds file and reset AI on seed run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# A list of product image links to seed the products wall
+product_images = [
+    'http://www.boerandfitch.com/8568-thickbox_default/funny-tshirts-get-real-be-rational.jpg',
+    'http://www.printscanada.com/file_library/userfiles/image/t-shirts-custom.jpg',
+    'http://digitalderek.typepad.com/sawxblog/photos/2007/playoffs/rockies_suck_tshirts.jpg'
+]
+
+# Remove all of the existing products
+Product.delete_all
+
+# TODO this will reset the SQLite autoincrement for products however this is specific to
+# to sql like and needs to be updated for a #dbmigration
+ActiveRecord::Base.connection.execute("delete from sqlite_sequence where name='products'")
+
+# Create some dummy products
+10.times do |i|
+  Product.create(name: "Product ##{i}", description: "A product.", price: rand(9.99...30.00), available: true, image_link: product_images.sample, purchase_link: 'google.ca')
+end
