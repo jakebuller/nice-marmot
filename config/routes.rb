8ad7e6912804356c8f3df838fbfd390cc2814b84
Jake Buller <jake.buller@enflick.com>
2015-04-02 21:56:37 -0400

    move products to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
 
   # Landing page setup
-  root 'welcome#index'
-  get 'welcome/index'
+  root 'products#index'
 
   # Product routes
-  # resources :products
   get 'products', to: 'products#index'
   get 'products/:id', to: 'products#show'
 end
