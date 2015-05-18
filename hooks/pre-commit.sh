00793eb5390b70e6ff60e745cdf5c70d462b300f
Jake Buller <jake.buller@enflick.com>
2015-05-18 18:51:52 -0400

    add rubocop hook

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -2,16 +2,11 @@
 # -------------------------------
 # Ruby Linter
 # -------------------------------
-files=$(git diff --cached --name-only --diff-filter=ACMR | grep "\.rb$")
-if [ "$files" != "" ]; then
-    if command -v rubocop >/dev/null 2>&1; then
-        rubocop -D -R ${files}
-        if [ $? != 0 ]; then
-          exit 1
-        fi
-    else
-        echo >&2 "rubocop is not installed"
-    fi
+echo 'Running rubocop'
+ruby $(cd "$(dirname "$0")" && pwd)/rubocop.rb
+if [ $? != 0 ]
+then
+  exit 1
 fi
 
 # -------------------------------
