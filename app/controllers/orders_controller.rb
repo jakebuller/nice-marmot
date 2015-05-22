c106cac9722501d895dc338ca795662b413f21b3
Jake Buller <jake.buller@enflick.com>
2015-05-21 22:48:16 -0400

    use locales for output strings

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,29 @@
 require_relative '../../lib/tee_shits_app'
 
 class OrdersController < ApplicationController
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  protect_from_forgery with: :null_session,
+                       if: proc { |c| c.request.format == 'application/json' }
 
-  # TODO dry up this class using before/after blocks to wrap each route for error handling
+  # TODO: dry up this class using before/after blocks to wrap each route for error handling
   def create
-    params.require(:email)
-    params.require(:stripeToken)
+    validate_create_params
 
     @app = TeeShits::App.create(params[:email])
     @app.create_order(params[:stripeToken])
 
     if @app.has_errors?
-      flash[:alert] = "Uh oh! It seems like we had some trouble placing your order: #{@app.error.message}"
+      flash[:alert] = I18n.t 'orders.error', error: "#{@app.error.message}"
       redirect_to :back
     else
-      flash[:notice] = "Awesome, you're order has been placed, you'll be getting funny looks in public before your know it!"
+      flash[:notice] = I18n.t 'orders.success'
       redirect_to products_path
     end
+  end
 
+  private
+
+  def validate_create_params
+    params.require(:email)
+    params.require(:stripeToken)
   end
 end
